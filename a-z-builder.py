ae5307583622b5780146471d2e6f7028137aa79b
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2018-08-15 15:16:22 +0100

    :scissors: Remove unnecessary dictionary initialisation

--- a-z-builder.py
+++ a-z-builder.py
@@ -6,6 +6,8 @@ words =f.read().splitlines()
 
 # the code below does not work as the empty array is a reference so one array is used by all keys
 # d = dict.fromkeys(string.ascii_lowercase, [])
+# this is the way to do it
+# d = { key : [] for key in keys }
 
 keyed_words = [ 
         {
@@ -14,15 +16,15 @@ keyed_words = [
         } for word in words if word.lstrip(string.punctuation) ]
 
 # build keys list from existing keys with a-z as minimum
-keys = set([ word['key'] for word in keyed_words ] + list(string.ascii_lowercase) )
-d = { key : [] for key in keys }
+# keys = set([ word['key'] for word in keyed_words ] + list(string.ascii_lowercase) )
 
-# build keys from a-z plus digits
-# allows for control keys to be deterministic but ...
+# build keys from a-z plus digits, allows for keys to be deterministic but ...
 # is possible for content editor to add a title which starts with a char not in the keys which will throw an error
-# d = { key : [] for key in string.ascii_lowercase + string.digits }
+keys = list(string.ascii_lowercase + string.digits)
 
-for key in d.keys():
+d = {}
+
+for key in keys:
     d[key] = sorted([ kw['word'] for kw in keyed_words if kw['key'] == key ])
 
 for key, value in sorted(d.items()):
